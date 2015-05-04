02d74f0c216180c75c3d89970d7177cc1389c6eb
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-05-04 21:43:07 +0200

    Nav directive links to some pages, index utilizes nav and meta tag for ! prefix

--- client/directives/nav/nav.directive.js
+++ client/directives/nav/nav.directive.js
@@ -1,9 +1,31 @@
 'use strict';
 
 angular.module('bangularSpike')
-  .directive('nav', function () {
+  .directive('nav', function ($location) {
     return {
       restrict: 'E',
       templateUrl: 'directives/nav/nav.html',
-    };
+      link: function(scope, element, attrs) { 
+        var links = element.find('a'),
+        onClass = attrs.nav || 'on',
+        link, url, currentLink, urlMap = {};
+
+        for(var i = 0; i < links.length; i++){
+          link = angular.element(links[i]);
+          url = link.attr('href');
+          urlMap[url] = link;
+        }
+
+        scope.$on('$routeChangeStart', function(){
+          var pathLink = urlMap[$location.path()];
+          if(pathLink){
+            if(currentLink){
+              currentLink.removeClass(onClass);
+            }
+            currentLink = pathLink;
+            currentLink.addClass(onClass);
+          }
+        });
+      }
+    }
   });
